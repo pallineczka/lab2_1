36ee6b451d6c25d6024b1da99335e4c8dd7fd2e7
pallineczka <paulina.stelmasiak97@gmail.com>
2019-03-19 19:34:29 +0100

    zad1

---
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.bsearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BinarySearchTest {
+
+    @Test
+    public void searchIsNull() throws Exception{
+
+    }
+}
