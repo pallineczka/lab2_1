539f229d8d1a9e427c3e78a7134d78a9adc082fb
pallineczka <paulina.stelmasiak97@gmail.com>
2019-03-25 23:14:08 +0100

    testy_v1 testElementIsFirstInSeq, testElementIsLastInSeq, testElementIsMiddleInSeq, testElementIsNotExistInSeq

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 public class BinarySearchTest {
 
-    @Test public void emptySeq() throws IllegalArgumentException {
+    @Test public void testEmptySeq() throws IllegalArgumentException {
         int[] seq = new int[0];
         int key = 5;
 
@@ -15,7 +15,7 @@ public class BinarySearchTest {
     }
 
     @Test
-    public void elementIsInSeq() {
+    public void testElementIsInSeq() {
         int[] seq = {5};
         int key = 5;
 
@@ -25,8 +25,48 @@ public class BinarySearchTest {
     }
 
     @Test
-    public void elementNotExistInSeq() {
+    public void testElementNotExistInSeq() {
         int[] seq = {9};
+        int key = 5;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        Assert.assertFalse(searchResult.isFound());
+        Assert.assertEquals(-1, searchResult.getPosition());
+    }
+
+    @Test
+    public void testElementIsFirstInSeq() {
+        int[] seq = {3, 6, 9};
+        int key = 3;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertEquals(0, searchResult.getPosition());
+    }
+
+    @Test
+    public void testElementIsLastInSeq() {
+        int[] seq = {3, 6, 9};
+        int key = 9;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertEquals(seq.length - 1, searchResult.getPosition());
+    }
+
+    @Test
+    public void testElementIsMiddleInSeq() {
+        int[] seq = {3, 6, 9};
+        int key = 6;
+
+        SearchResult searchResult = BinarySearch.search(key, seq);
+        Assert.assertTrue(searchResult.isFound());
+        Assert.assertEquals((seq.length - 1)/2, searchResult.getPosition());
+    }
+
+    @Test
+    public void testElementIsNotExistInSeq() {
+        int[] seq = {3, 6, 9};
         int key = 5;
 
         SearchResult searchResult = BinarySearch.search(key, seq);
